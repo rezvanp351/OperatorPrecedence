0a2cd1e9e6d9b0eda2a4b7bfbcb263e566d5dbfc
Mohd R. Panah <rezvanpanah4@gmail.com>
2025-09-21 13:13:54 +0430

    Add files via upload

---
+++ OperatorPrecedence.py
@@ -0,0 +1,18 @@
+# examples.py
+print((2 + 3) * 4)           # 20
+print(2 + 3 * 4)             # 14
+print(2 ** 3 ** 2)           # 512
+print(-3 ** 2)               # -9
+print((-3) ** 2)             # 9
+expr = 2 + 3 * 4 ** 2 // (1 + 1) - -5
+print(expr)                  # 31
+
+# More examples showing logical operator behavior
+print(0 or 5)                # 5
+print(1 and 2)               # 2
+print(not 0)                 # True
+
+# Bitwise examples
+print(5 & 3)                 # 1
+print(5 ^ 3)                 # 6
+print(5 | 3)                 # 7
